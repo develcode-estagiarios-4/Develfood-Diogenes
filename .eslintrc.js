c618f1af5eea08df97c6ce2d31b3c8f792588078
Diogenes Barros <diogenes.barros@develcode.com.br>
2022-04-27 09:46:01 -0300

    first commit

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,20 @@
 module.exports = {
   root: true,
   parser: "@typescript-eslint/parser",
-  plugins: [
-    "@typescript-eslint"
-  ],
+  plugins: ["@typescript-eslint"],
+  rules: {
+    "@typescript-eslint/rule-name": "error",
+    "@typescript-eslint/adjacent-overload-signatures": "error",
+    "@typescript-eslint/no-empty-interface": [
+      "error",
+      {
+        "allowSingleExtends": false
+      }
+    ],
+  },
   extends: [
-    'airbnb-typescript',
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:@typescript-eslint/recommended-requiring-type-checking"
   ]
-}
+};
